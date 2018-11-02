595626980f80ae0ea7b4bde5530802ab36b940a0
MitchellHenr <henry@tom.org>
2018-11-02 15:29:43 -0400

    Improved runner

--- code/runner.py
+++ code/runner.py
@@ -16,19 +16,27 @@ def qsub(α, β):
     subprocess.Popen(["qsub", "-q", "shortq", "submit.pbs"])
 
 
-def auto_run(max_jobs=55):
+def auto_run(max_jobs=200):
     jobs = max(0, int(subprocess.check_output('qstat | wc -l', shell=True)) - 2)
     print("Jobs: ", jobs)
     for params in product(linspace(0, 1.6, 80), linspace(0, 0.4, 20)):
         α, β = params
         if f"{α:.03f}-{β:.03f}.pkl" not in os.listdir("../data"):
+            print("Jobs: ", jobs)
             print(α, β)
+            jobs = max(0, int(subprocess.check_output('qstat | wc -l', shell=True)) - 2)
             while jobs >= max_jobs:
                 time.sleep(60)
                 jobs = max(0, int(subprocess.check_output('qstat | wc -l', shell=True)) - 2)
-                print("Jobs: ", jobs)
             qsub(α, β)
             time.sleep(10)
 
 
 auto_run()
+
+jobs = max(0, int(subprocess.check_output('qstat | wc -l', shell=True)) - 2)
+while jobs != 0:
+    jobs = max(0, int(subprocess.check_output('qstat | wc -l', shell=True)) - 2)
+    time.sleep(10)
+print("Done")
+subprocess.Popen(["qsub", 'hizandis.pbs'])
