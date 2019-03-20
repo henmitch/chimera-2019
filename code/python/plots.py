53cabe0b9819a7d65378e22389ac4eb160810805
Henry Mitchell <henry@henrymitchell.org>
2019-03-19 23:20:09 -0400

    Added plot-all to plots

--- code/python/plots.py
+++ code/python/plots.py
@@ -56,6 +56,8 @@ plt.clf()
 
 plt.plot(np.cos(np.mean(phase, axis=1)))
 plt.title(title)
+plt.xlim([0, phase.shape[0]])
+plt.ylim([-1.05, 1.05])
 plt.xlabel("time")
 plt.ylabel("cos(mean(phase))")
 fig = plt.gcf()
@@ -69,6 +71,8 @@ for cortex in cortices:
     [low, high] = cortex
     plt.plot(np.cos(np.mean(phase[:, low:high], axis=1)),
              label=f"{low + 1}-{high}", lw=0.05)
+plt.xlim([0, phase.shape[0]])
+plt.ylim([-1.05, 1.05])
 plt.xlabel("time")
 plt.ylabel("cos(mean(phi))")
 plt.legend(bbox_to_anchor=(1, 1))
@@ -81,6 +85,7 @@ plt.cla()
 plt.clf()
 
 plt.plot(order(phase))
+plt.xlim([0, phase.shape[0]])
 plt.xlabel("time")
 plt.ylabel("rho")
 plt.title(title)
@@ -95,6 +100,7 @@ for cortex in cortices:
     [low, high] = cortex
     plt.plot((order(phase[:, low:high]) - ρ_bar)**2,
              label=f"{low + 1}-{high}", lw=0.05)
+plt.xlim([0, phase.shape[0]])
 plt.xlabel("time")
 plt.ylabel("(rho - rho bar)**2")
 plt.legend(bbox_to_anchor=(1, 1))
@@ -107,12 +113,46 @@ plt.cla()
 plt.clf()
 
 plt.plot(chi)
+plt.xlim([0, phase.shape[0]])
 plt.xlabel("time")
 plt.ylabel("sigma(chi(t))")
 plt.title(title)
 fig = plt.gcf()
 fig.set_size_inches(*size, forward=True)
 plt.savefig(f"../figure/chi-{α:.03f}-{β:.03f}.png", dpi=500,
+            bbox_inches="tight", format="png")
+plt.cla()
+plt.clf()
+
+fig, [overhead_ax,
+      order_ax,
+      cos_mean_ax,
+      by_cortex_cos_mean_ax,
+      variance_ax,
+      by_cortex_variance_ax] = plt.subplots(6, 1, sharex=True, squeeze=True)
+overhead_ax.set_xlim([0, phase.shape[0]])
+
+overhead_ax.matshow(phase.T, aspect="auto", origin="lower")
+
+cos_mean_ax.plot(np.cos(np.mean(phase, axis=1)), lw=0.1)
+
+for cortex in cortices:
+    [low, high] = cortex
+    by_cortex_cos_mean_ax.plot(np.cos(np.mean(phase[:, low:high], axis=1)),
+                            label=f"{low + 1}-{high}", lw=0.07)
+
+order_ax.plot(order(phase), lw=0.1)
+order_ax.set_ylim([])
+
+variance_ax.plot(chi, lw=0.1)
+
+for cortex in cortices:
+    [low, high] = cortex
+    by_cortex_variance_ax.plot(((order(phase[:, low:high]) - ρ_bar)**2),
+                            label=f"{low + 1}-{high}", lw=0.07)
+
+fig.set_size_inches(*size, forward=True)
+plt.savefig(f"../figure/all-{α:.03f}-{β:.03f}.png", dpi=500,
             bbox_inches="tight", format="png")
 plt.cla()
 plt.clf()
