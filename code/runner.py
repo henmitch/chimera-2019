a2e77d1de2141ef1d6c18571304496139d36ed88
MitchellHenr <henry@tom.org>
2019-03-26 08:56:57 -0400

    Made runner more general

--- code/runner.py
+++ code/runner.py
@@ -19,14 +19,14 @@ def qsub(template, args):
 def auto_run(template, args, max_jobs=200):
     for params in args:
         jobs = max(0, int(subprocess.check_output(
-            "qstat | wc -l", shell=True)) - 2)
+            "qstat | grep ' [RQ] ' | wc -l", shell=True)) - 2)
         print("Jobs: ", jobs)
         while jobs >= max_jobs:
             time.sleep(30)
             jobs = max(0, int(subprocess.check_output(
-                "qstat | wc -l", shell=True)) - 2)
+                "qstat | grep ' [RQ] ' | wc -l", shell=True)) - 2)
         qsub(template, params)
-        time.sleep(5)
+        time.sleep(1)
 
 
 def main():
@@ -37,23 +37,28 @@ def main():
         "t", metavar="type", type=str, nargs=1,
         help="The type of job to run.  Either 'calc', 'plot', or 'fix'."
     )
+    parser.add_argument(
+        "-d", metavar="data_dir", type=str, nargs=1, default="../data",
+        help="The location to which to save the data"
+    )
     c_args = parser.parse_args()
+    data_dir = c_args.d[0]
 
     if c_args.t[0] == "calc":
         template_file = "submit_scripts/template.pbs"
-        args = [[α, β, α, β]
+        args = [[α, β, α, β, data_dir]
                 for α, β
-                in product(linspace(0.0, 0.2, 80), linspace(0.0, 0.1, 40))
-                if f"{α:0.3f}-{β:0.3f}.pkl" not in os.listdir("../data/zoom")]
+                in product(linspace(0.0, 0.9, 80), linspace(0.0, 0.9, 80))
+                if f"{α:.05f}-{β:.05f}.pkl" not in os.listdir(data_dir)]
     elif c_args.t[0] == "plot":
         template_file = "submit_scripts/plots_template.pbs"
-        with open("../data/hizanidis_params.pkl", "rb") as f:
+        with open(f"{data_dir}/hizanidis_params.pkl", "rb") as f:
             params = pickle.load(f)
         good = params[params["max_phase"] <= 2*pi]
-        args = [[f"{α:.03f}_{β:.03f}", f"{α:.03f}-{β:.03f}.pkl"]
+        args = [[f"{α:.05f}_{β:.05f}", f"{α:.05f}-{β:.05f}.pkl"]
                 for row, [α, β]
                 in good[["alpha", "beta"]].iterrows()
-                if all([(f"{front}-{α:.03f}-{β:.03f}.png"
+                if all([(f"{front}-{α:.05f}-{β:.05f}.png"
                          not in os.listdir("figure"))
                         for front in [
                     "overhead",
@@ -68,7 +73,7 @@ def main():
         template_file = "submit_scripts/fix_template.pbs"
         args = [(i.rstrip(), i.rstrip())
                 for i
-                in os.listdir("../data/")]
+                in os.listdir(data_dir)]
     elif c_args.t[0] == "network":
         template_file = "submit_scripts/network_template.pbs"
         args = [(t, t)
