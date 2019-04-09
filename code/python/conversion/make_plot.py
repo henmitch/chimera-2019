4bc9231fdec393663cc6ab0e83556dc81afcc8aa
Henry Mitchell <henry@henrymitchell.org>
2019-04-08 22:42:39 -0400

    Plot phase instead of potential in network

--- code/python/conversion/make_plot.py
+++ code/python/conversion/make_plot.py
@@ -9,12 +9,12 @@ parser.add_argument(
     help="The timestep at which to plot"
 )
 parser.add_argument(
-    "-min", metavar="min", type=float, nargs=1, default=-1.5,
-    help="The minimum potential value (defaults to -1.5)"
+    "-min", metavar="min", type=float, nargs=1, default=-np.pi,
+    help="The minimum potential value (defaults to -pi)"
 )
 parser.add_argument(
-    "-max", metavar="max", type=float, nargs=1, default=2,
-    help="The maximum potential value (defaults to 2)"
+    "-max", metavar="max", type=float, nargs=1, default=np.pi,
+    help="The maximum potential value (defaults to pi)"
 )
 parser.add_argument(
     "f", metavar="file", type=str, nargs=1,
@@ -41,5 +41,7 @@ potentials = np.array([G.nodes[i]["potential"][t][0] for i in G.nodes])
 nx.draw_networkx(G, pos=pos, node_size=node_size, arrowsize=1, width=0.05,
                  edge_color="#777777", node_color=potentials,
                  with_labels=False, vmin=args.min, vmax=args.max)
-plt.xlabel(t)
+plt.xticks([])
+plt.yticks([])
+
 plt.savefig(f"animated/{t:04}.png", dpi=100)
