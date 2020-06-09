53fedc3a2698aee2c6c9186e19b10bd8bb061ed6
Henry Mitchell <henry@henrymitchell.org>
2020-06-09 14:18:36 -0400

    Made revisions

--- IJBC/auto/lit_review.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "lit_review"
- (lambda ()
-   (TeX-run-style-hooks
-    "chapter/lit_review_section/chimera"))
- :latex)
-
--- IJBC/auto/seizures.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "seizures"
- (lambda ()
-   (LaTeX-add-labels
-    "sec:intro_seizures_aetiology"))
- :latex)
-
